0909157c03e6262d6406f98c34f94529d0f15ea1
yankas <christopher.sbrisny@gmail.com>
2024-11-14 15:06:27 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd "${0%/*}"
+mkdir ./out
+cp -r ./src/* ./out/
+
---
+++ setup.sh
@@ -0,0 +1,3 @@
+cd "${0%/*}"
+mkdir -p $HOME/games/Diablo\ II\ Resurrected/Mods/brimstone/
+ln -s $(realpath ./out) $HOME/games/Diablo\ II\ Resurrected/Mods/brimstone/brimstone.mpq
